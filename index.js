84b40cd88b974d1dc23068e80126a57ec8d17bba
Jonathan Moreno <johnfrom209@gmail.com>
2022-10-24 19:41:36 -0700

    questions for the prompt and added files for the different classes(not filled)

---
+++ index.js
@@ -0,0 +1,46 @@
+
+let managerQuestions = [
+    {
+        name: 'teamManager',
+        message: 'what is the team manager\'s name?'
+    },
+    {
+        name: 'id',
+        message: 'What is the id number?'
+    },
+    {
+        name: 'offcieNumber',
+        message: 'What is the office number?'
+    }
+];
+// seems kinda dry in here
+let engineerQuestions = [
+
+    {
+        name: 'Name',
+        message: 'What is the engineer\s name?'
+    },
+    {
+        name: 'email',
+        message: 'What is their email?'
+    },
+    {
+        name: 'github',
+        message: 'What is their github?'
+    }
+];
+
+let internQuestions = [
+    {
+        name: 'name',
+        message: 'What is their name?'
+    },
+    {
+        name: 'id',
+        message: 'What is their id number?'
+    },
+    {
+        name: 'school',
+        message: 'What school are they attending?'
+    }
+]
